e6a219c70af455e2015129ff32b7692f6b1dffdf
huojunxin <huojunxin@bytedance.com>
2020-04-29 17:19:59 +0800

    表结构测试

--- define.go
+++ define.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"time"
@@ -43,6 +44,46 @@ func main() {
 		panic(err)
 	}
 	defer db.Close()
+	db.LogMode(true)
 
+	// 创建表、更新表结构
 	db.AutoMigrate(&User{}, &User2{}, &Animal{})
+
+	// 插入
+	//t, _ := time.ParseInLocation("2006-01-02 15:04:05", "2019-06-18 08:47:50", time.Local)
+	//num := "887799669"
+	//u1 := User{
+	//	Name:         "aaa",
+	//	Age:          sql.NullInt64{25, true},
+	//	Birthday:     &t,
+	//	Email:        "1234@qq.com",
+	//	Role:         "Student",
+	//	MemberNumber: &num,
+	//	Address:      "qqwwaass",
+	//	IgnoreMe:     666,
+	//}
+	//db.Create(&u1)
+
+	// 查询
+	var u2 []User
+	db.Find(&u2, "role=?", "Student")
+	fmt.Printf("%#v\n", u2)
+
+	var u3 User
+	db.Where("email=?", "123@qq.com").First(&u3)
+	fmt.Printf("%#v\n", u3)
+
+	//u3.Age = sql.NullInt64{26, true}
+	//// model为实例，更新该条记录
+	//db.Model(u3).Updates(User{
+	//	Name: "Steven",
+	//})
+
+	// model为类型，更新全表
+	//db.Model(User{}).Updates(User{
+	//	Name: "Bob",
+	//})
+
+	// 删除（delete_at字段设置当前时间，并未删除，软删除）
+	db.Delete(u3)
 }
